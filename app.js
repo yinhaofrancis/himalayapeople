f4bdd6c107e4618d2b07b3ac5780ee65920bffa2
Francis <1833918721@qq.com>
2020-06-26 02:43:35 +0800

    up

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var helmet = require('helmet');
+var compression = require('compression');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+app.use(compression())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
